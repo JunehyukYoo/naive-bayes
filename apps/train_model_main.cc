5e9114edf294baaaf2a0e4788f4c34f383efdd75
junehyukyoo <yoo.junehyuk@gmail.com>
2021-04-04 22:32:47 -0500

    >> operator works as it adds in the correct data into the savefile.txt, although needs testing for accuracy.

--- apps/train_model_main.cc
+++ apps/train_model_main.cc
@@ -7,12 +7,20 @@
 int main() {
   std::string file_path_test = "/Users/s200808/Documents/Cinder/my-projects/naive-bayes/data/testtrainingimages.txt";
   std::string file_path_full = "/Users/s200808/Documents/Cinder/my-projects/naive-bayes/data/trainingimagesandlabels.txt";
-  naivebayes::DataModel model;
-  std::ifstream input_file(file_path_full);
+  std::string file_path_save = "/Users/s200808/Documents/Cinder/my-projects/naive-bayes/data/savefile.txt";
+  naivebayes::DataModel model(3);
+  std::ifstream input_file(file_path_test);
   if (input_file.is_open()) {
     input_file >> model;
   } else {
     std::cerr << "error_message" << std::endl;
+  }
+  
+  std::ofstream output_file(file_path_save);
+  if (output_file.is_open()) {
+    output_file << model;
+  } else {
+    std::cerr << "error message" << std::endl;
   }
   return 0;
 }
